cac3ee6661fdd1e0b63ab49ea9b040d92d01b755
Manda-supraja26 <mandasupraja1365139.com>
2022-12-26 23:34:33 +0530

    26/12/2022

--- GoLang/20 Urls/main.go
+++ GoLang/20 Urls/main.go
@@ -5,18 +5,39 @@ import (
 	"net/url"
 )
 
-const myurl = "https://lco.dev:3000/learn?coursename=reactjs&price=230"
+const Myurl string = "https://lco.dev:3000/learn?coursename=reactjs&payment=gbhvalue120"
 
 func main() {
-	fmt.Println("Working with Urls")
-	fmt.Println(myurl)
+	fmt.Println("URL's In Golang")
+	fmt.Println(Myurl)
+	fmt.Printf("The type of Url is : %T", Myurl)
 
 	// parsing
-	result, _ := url.Parse(myurl)
+	content, _ := url.Parse(Myurl)
+	fmt.Println(content.Scheme)
+	fmt.Println(content.Host)
+	fmt.Println(content.Path)
+	fmt.Println(content.RawQuery)
+	fmt.Println(content.Port())
 
-	fmt.Println(result)
-	fmt.Println(result.Scheme)
-	fmt.Println(result.Path)
-	fmt.Println(result.RawQuery)
-	fmt.Println(result.Port())
+	// the RawQuery data is stored in form of map and we can acces it via
+	data := content.Query()
+	fmt.Println(data)
+	// fmt.Printf("print %T", data["coursename"])
+
+	for _, val := range data {
+		fmt.Println("Param is ", val)
+	}
+
+	urlInfo := &url.URL{
+		Scheme:  "https",
+		Host:    "lco.dev",
+		Path:    "/tutcss",
+		RawPath: "user=supraja",
+	}
+	anotherurl := urlInfo.String()
+	fmt.Println(anotherurl)
+
+	// now we trying to create a url based info
+
 }
